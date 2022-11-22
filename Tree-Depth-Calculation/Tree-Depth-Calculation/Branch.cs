1c175a34496f921454ca9ed4b903c8fc9b964bc7
TadisOn <76095383+TadisOn@users.noreply.github.com>
2022-11-22 13:31:25 +0200

    Tree depth Calculation

---
+++ Tree-Depth-Calculation/Tree-Depth-Calculation/Branch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_Depth_Calculation
+{
+    internal class Branch
+    {
+        List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+        public void Add(Branch br)
+        {
+            this.branches.Add(br);
+        }
+
+        public int GetDepth()
+        {
+            return GetTreeDepth(this, 1);
+        }
+
+        public int GetTreeDepth(Branch br, int curr_depth)
+        {
+            int res = curr_depth;
+            foreach (Branch branch in br.branches)
+            {
+                res = Math.Max(res, GetTreeDepth(branch, curr_depth + 1));
+            }
+            return res;
+        }
+    }
+}
---
+++ Tree-Depth-Calculation/Tree-Depth-Calculation/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Tree_Depth_Calculation;
+               
+
+//Given example in pdf
+
+Branch main = new Branch();
+
+//Main has 2 branches
+Branch b1 = new Branch();
+Branch b2 = new Branch();
+main.Add(b1);
+main.Add(b2);
+
+//b1 has 1 branch
+Branch b1_1 = new Branch();
+b1.Add(b1_1);
+
+//b2 has 3 branches
+Branch b2_1 = new Branch();
+Branch b2_2 = new Branch();
+Branch b2_3 = new Branch();
+b2.Add(b2_1);
+b2.Add(b2_2);
+b2.Add(b2_3);
+
+//b2_1 has 1 branc
+Branch b2_1_1 = new Branch();
+b2_1.Add(b2_1_1);
+
+//b2_2 has 2 branches
+Branch b2_2_1 = new Branch();
+Branch b2_2_2 = new Branch();
+b2_2.Add(b2_2_1);
+b2_2.Add(b2_2_2);
+
+//b2_2_1 has 1 branch
+Branch b2_2_1_1 = new Branch();
+b2_2_1.Add(b2_2_1_1);
+
+Console.WriteLine("Tree Depth: {0}", main.GetDepth());
+
+
